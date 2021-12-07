7001740ad167f1a53441323ed88a47c3fa680c10
Torster <aleksander.klemba@lo31.krakow.pl>
2021-12-07 22:23:44 +0100

    Delete # II zad 4 (u)(bez samej liczby).py

--- # II zad 4 (u)(bez samej liczby).py
+++
@@ -1,8 +0,0 @@
-# II zad 4 (u)(bez samej liczby)
-liczby = int(input("Wprowadź liczbę:"))
-dzielniki = []
-
-for i in range(1, liczby):
-    if liczby % i ==0:
-        dzielniki.append(i)
-print(dzielniki)
